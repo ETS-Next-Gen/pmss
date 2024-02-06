d6baac0ea682785f9bae9c8a5d18c73ee460e37c
Piotr Mitros <piotr@mitros.org>
2024-02-06 15:41:42 -0500

    css_interface.py (frivolous commit; syncing between machines mid-dev)

--- css_interface.py
+++ css_interface.py
@@ -1,2 +1,14 @@
 import tinycss2
 
+with open('creds.pss.example', 'r') as fd:
+    pss = fd.read()
+
+rules = tinycss2.parse_stylesheet(
+    pss,
+    skip_whitespace=True,
+    skip_comments=True
+)
+for rule in rules:
+    print("R>", rule)
+    print("P>", rule.prelude)
+    print("C>", rule.content)
