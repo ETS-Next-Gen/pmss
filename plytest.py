209be656ce5ba26c5e13c14440087c12d2126ff1
Piotr Mitros <piotr@mitros.org>
2024-02-18 15:23:43 -0500

    W3C macros. About to refactor to a clean version

--- plytest.py
+++ plytest.py
@@ -31,6 +31,45 @@ tokens = (
     'DELIM'
 )
 
+# Taken from W3.org, and brought in by hand. Moving away from GPT-generated code
+macros = {
+    "ident": r"[-]?{nmstart}{nmchar}*",
+    "name": r"{nmchar}+",
+    "nmstart": r"[_a-z]|{nonascii}|{escape}",
+    "nonascii": r"[^\0-\237]",
+    "unicode": r"\\[0-9a-f]{1,6}(\r\n|[ \n\r\t\f])?",
+    "escape": r"{unicode}|\\[^\n\r\f0-9a-f]",
+    "nmchar": r"[_a-z0-9-]|{nonascii}|{escape}",
+    "num": r"[0-9]+|[0-9]*\.[0-9]+",
+    "string": r"{string1}|{string2}",
+    "string1": '\"([^\n\r\f\\"]|\\{nl}|{escape})*\"',
+    "string2": r"\'([^\n\r\f\\']|\\{nl}|{escape})*\'",
+    "badstring": r"{badstring1}|{badstring2}",
+    "badstring1": '\"([^\n\r\f\\"]|\\{nl}|{escape})*\\?',
+    "badstring2": r"\'([^\n\r\f\\']|\\{nl}|{escape})*\\?",
+    "badcomment": r"{badcomment1}|{badcomment2}",
+    "badcomment1": r"\/\*[^*]*\*+([^/*][^*]*\*+)*",
+    "badcomment2": r"\/\*[^*]*(\*+[^/*][^*]*)*",
+    "baduri": r"{baduri1}|{baduri2}|{baduri3}",
+    "baduri1": r"url\({w}([!#$%&*-~]|{nonascii}|{escape})*{w}",
+    "baduri2": r"url\({w}{string}{w}",
+    "baduri3": r"url\({w}{badstring}",
+    "nl": r"\n|\r\n|\r|\f",
+    "w": r"[ \t\r\n\f]*",
+    "1,6": "{1,6}"  # <-- Dummy, so expansion doesn't break on {1,6}
+}
+
+import json
+
+def expand_macros():
+    global macros
+    for i in range(3):
+        macros = {key: value.format(**macros) for key, value in macros.items()}
+
+expand_macros()
+
+print(json.dumps(macros, indent=2))
+
 # Regular expression rules for simple tokens
 # These were AI-assisted, based on W3C specs:
 
@@ -38,7 +77,6 @@ tokens = (
 
 # They have a few bugs, and need a few test cases. But they're a fine
 # starting point.
-
 t_IDENT = r'[a-zA-Z_][a-zA-Z0-9_-]*'
 t_ATKEYWORD = r'@' + t_IDENT
 t_STRING = r'\"[^\"]*\"|\'' + r'[^\']*\''
@@ -63,7 +101,7 @@ t_LBRACKET = r'\['
 t_RBRACKET = r'\]'
 t_WHITESPACE = r'[\t\r\n\f ]+'
 t_COMMENT = r'\/\*[^*]*\*+([^/*][^*]*\*+)*\/'
-t_COMMENT = r'\/\*([^\*]|(\*[^\/]))*\*+\/'
+#t_COMMENT = r'\/\*([^\*]|(\*[^\/]))*\*+\/'
 t_FUNCTION = t_IDENT + r'\('
 t_INCLUDES = r'~='
 t_DASHMATCH = r'\|='
