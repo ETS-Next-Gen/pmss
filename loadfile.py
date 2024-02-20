9fd83e2a4bf9f5abc81ca5e420f73981d2594142
Piotr Mitros <piotr@mitros.org>
2024-02-20 12:23:48 -0500

    Missing yield_from and other fixes

--- loadfile.py
+++ loadfile.py
@@ -4,18 +4,19 @@ import re
 import pssyacc
 import psslex
 
-import selectors
+import pssselectors
 
 
-def flatten_rules(block_list, parent_selector=selectors.NullSelector()):
+def flatten_rules(block_list, parent_selector=pssselectors.NullSelector()):
     for selector, block in block_list:
         if block is None:
             continue
         for rule in block:
-            if not isinstance(rule[0], selectors.Selector):
-                yield parent_selector + selector, rule[0], rule[1]
+            combined_selector = parent_selector + selector
+            if not isinstance(rule[0], pssselectors.Selector):
+                yield combined_selector, rule[0], rule[1]
             else:
-                flatten_rules([rule], parent_selector + selector)
+                yield from flatten_rules([rule], combined_selector)
 
 
 def flatten_and_print_parse(parse_results):
@@ -36,6 +37,7 @@ def load_pss_file(filename, print_debug=False):
     text = open(filename).read()
     no_comments = psslex.strip_comments(text)
     result = pssyacc.parser.parse(no_comments, lexer=psslex.lexer)
+    print(result)
     if print_debug:
         flatten_and_print_parse(result)
     rules = rule_sheet(result)
